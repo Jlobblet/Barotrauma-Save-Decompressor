fb1baa4bb2a6182efe8d7377246a2d41cb349da4
Jlobblet <jlobblet@gmx.com>
2020-09-03 22:07:56 +0100

    Add CLI interface

---
+++ Barotrauma-Save-Decompressor-CLI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CommandLine;
+
+namespace Barotrauma_Save_Decompressor_CLI
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        private static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<CompressOptions, DecompressOptions>(args)
+                  .MapResult((CompressOptions o) => CompressOptions.Run(o),
+                             (DecompressOptions o) => DecompressOptions.Run(o),
+                             err => 1);
+        }
+    }
+}
